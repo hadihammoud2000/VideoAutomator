1fdbf49ec7eb60be4d0cc9b31f28ef105dfef166
Hadi Hamoud <hadihamoud@Hadis-MacBook-Pro.local>
2022-05-23 22:24:12 +0300

    initial commit

---
+++ Starters/starter.py
@@ -0,0 +1,4 @@
+from moviepy.editor import *
+clip = VideoFileClip("/Users/hadihamoud/Desktop/Projects/VideoAutomator/vauto/lib/python3.9/site-packages/imageio/resources/images/realshort.mp4").rotate(180)
+clip.ipython_display(width=280)
+
